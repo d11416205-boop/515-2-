e6541b024321e645d7521b23fb27912de2e5dcc3
Administrator <Administrator@A316-22>
2026-05-15 12:12:57 +0800

    加入專案檔案。

---
+++ 515-2-/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _515_2_
+{
+    {
+
+    internal class Program
+        {
+
+    static void Main(string[] args)
+            {
+                IName p1 = new Student();
+                Console.WriteLine(p1.GetName());
+            }
+        }
+    }
